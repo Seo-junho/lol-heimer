da51d0d348660be0ea6831f7e123190b3d3974c1
jun_ho98 <jun_ho98@naver.com>
2020-12-06 03:02:50 +0900

    search user sample

--- application/controllers/api/Search.php
+++ application/controllers/api/Search.php
@@ -2,9 +2,35 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Search extends CI_Controller{
+	private $base_url = 'https://kr.api.riotgames.com/lol/';
+	private $api_key = 'RGAPI-9f2cc058-bc56-4a84-9774-2255676d79da';
 
 	public function index(){
-		$API_KEY = 'RGAPI-9f2cc058-bc56-4a84-9774-2255676d79da';
+		echo 'index_page';
+	}
+
+	public function user(string $userId)
+	{
+		if (empty($userId)) {
+			echo 'not found id';
+			return false;
+		}
+		$api_url = $this->base_url . '/summoner/v4/summoners/by-name/?api_key' . $this->api_key;
+
+		$data = [
+			'summonerName' => $userId
+		];
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $api_url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+		$response = curl_exec($ch);
+		curl_close($ch);
+
+		return json_encode($response);
 	}
 
 }
