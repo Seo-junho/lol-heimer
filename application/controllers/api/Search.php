1533fcfd5543dc2ba4aba0ef8dee2c316eac749a
jun_ho98 <jun_ho98@naver.com>
2020-12-06 03:03:37 +0900

    Update Search.php

--- application/controllers/api/Search.php
+++ application/controllers/api/Search.php
@@ -24,8 +24,10 @@ class Search extends CI_Controller{
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $api_url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($ch, CURLOPT_POST, true);
 
 		$response = curl_exec($ch);
 		curl_close($ch);
