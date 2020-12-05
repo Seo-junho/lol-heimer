257fea063abada0b968879c5824149537b5fd86a
jun_ho98 <jun_ho98@naver.com>
2020-12-06 02:53:02 +0900

    Create Search.php

---
+++ application/controllers/api/Search.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Search extends CI_Controller{
+
+	public function index(){
+		$API_KEY = 'RGAPI-9f2cc058-bc56-4a84-9774-2255676d79da';
+	}
+
+}
+
+?>
