a83b3ab5413d8baa11107a5f08f53f6f1bec7a62
jun_ho98 <jun_ho98@naver.com>
2020-12-06 04:12:14 +0900

    Update Search.php

--- application/controllers/api/Search.php
+++ application/controllers/api/Search.php
@@ -9,28 +9,119 @@ class Search extends CI_Controller{
 		echo 'index_page';
 	}
 
-	public function user(string $userId)
+	/**
+	 * 유저의 기본 정보를 가져옴
+	 * @param $userName
+	 * @return false|string
+	 */
+	public function getUserInfo($userName)
 	{
-		if (empty($userId)) {
-			echo 'not found id';
-			exit();
-		}
-		$api_url = $this->base_url . '/summoner/v4/summoners/by-name/'. $userId . '?api_key=' . $this->api_key;
+		$response['code'] = 200;
+		$response['message'] = 'Success';
+
+		$userId = $this->getUserId($userName);
+		$league_info = $this->getUserLeagueInfo($userId);
+
+		$response['data']['user_info'] = $this->getUser($userName);
+		$response['data']['solo_league_info'] = $league_info[0];
+		$response['data']['team_league_info'] = $league_info[1];
+
+		return json_encode($response);
+	}
+
+	/**
+	 * 현재 서버 상태를 가져옴
+	 * @return mixed
+	 */
+	public function getServerStatus()
+	{
+		$response['code'] = 200;
+		$response['message'] = 'Success';
+		$response['data'] = [];
+
+
+		$api_url = $this->base_url . '/status/v4/platform-data?api_key=' . $this->api_key;
 
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $api_url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+		$result = curl_exec($ch);
+		curl_close($ch);
+
+		$response['data'] = $result;
+
+		return json_encode($response);
+	}
+	
+	/**
+	 * 유저의 리그 정보를 가져옴
+	 * @param string $encryptedSummonerId 유저 고유 id
+	 * @return bool|string
+	 */
+	public function getUserLeagueInfo(string $encryptedSummonerId)
+	{
+		$api_url = $this->base_url . '/league/v4/entries/by-summoner/'. $encryptedSummonerId . '?api_key=' . $this->api_key;
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $api_url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+		$result = json_decode(curl_exec($ch));
+		curl_close($ch);
+
+		return $result;
+	}
+
+	/**
+	 * 유저의 고유정보를 가져옴
+	 * @param string $userName 유저 고유 id
+	 * @return mixed
+	 */
+	private function getUser(string $userName)
+	{
+		$api_url = $this->base_url . '/summoner/v4/summoners/by-name/'. $userName . '?api_key=' . $this->api_key;
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $api_url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		//curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 		//curl_setopt($ch, CURLOPT_POST, true);
 
-		$response = curl_exec($ch);
+		$result = curl_exec($ch);
 		curl_close($ch);
 
-		var_dump($response);
+		return $result;
 	}
 
+	/**
+	 * 유저의 아이디를 가져옴
+	 * @param string $userName 유저명
+	 * @return mixed
+	 */
+	private function getUserId(string $userName)
+	{
+		$api_url = $this->base_url . '/summoner/v4/summoners/by-name/'. $userName . '?api_key=' . $this->api_key;
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $api_url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		//curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		//curl_setopt($ch, CURLOPT_POST, true);
+
+		$result = json_decode(curl_exec($ch));
+		curl_close($ch);
+
+		return $result->id;
+	}
 }
 
 ?>
