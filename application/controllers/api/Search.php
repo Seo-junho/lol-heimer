7a9420f7faa1affdb43b7a72168290fa56b98298
jun_ho98 <jun_ho98@naver.com>
2020-12-26 18:11:03 +0900

    Update Search.php

--- application/controllers/api/Search.php
+++ application/controllers/api/Search.php
@@ -24,9 +24,10 @@ class Search extends CI_Controller{
 		$response['code'] = 200;
 		$response['message'] = 'Success';
 		$response['data']['user_info'] = $this->getUser($userName);
-		$response['data']['solo_league_info'] = $league_info[0];
-		$response['data']['team_league_info'] = $league_info[1];
-
+		if (!empty($league_info)) {
+			$response['data']['solo_league_info'] = $league_info[0];
+			$response['data']['team_league_info'] = $league_info[1];
+		}
 		echo json_encode($response);
 	}
 
