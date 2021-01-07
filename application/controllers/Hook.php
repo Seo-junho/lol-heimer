57df7f0c9b73dbe976a655e79ceec3be00873c06
junho.seo <jun_ho98@naver.com>
2021-01-08 03:28:13 +0900

    Update Hook.php (#3)

--- application/controllers/Hook.php
+++ application/controllers/Hook.php
@@ -4,8 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Hook extends CI_Controller {
 	public function index(){
-		$id = $this->input->get_post("id");
-		$data = $this->input->get_post("data");
+		$id = $_POST("id");
+		$data = $_POST("data");
 
 		var_dump($id);
 		var_dump($data);
