b0d0673e384670c572a46467443a534e6aa49f7c
zoz0312 <zoz0312@naver.com>
2021-01-08 00:14:44 +0900

    fix sh

--- heroku.sh
+++ heroku.sh
@@ -1,5 +1,9 @@
 cd lol-heimer-frontend ;\
+echo "start npm install";\
 npm install ;\
+echo "end npm install";\
+echo "================";\
+echo "start build";\
 npm run build ;\
 mv ./lol-heimer-frontend/build/index.html ./application/views/home.html ;\
 mv ./lol-heimer-frontend/build/* ./ ;\
