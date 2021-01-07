071d9e138ac94bd18745bcb4e104165b0c2facc0
zoz0312 <zoz0312@naver.com>
2021-01-07 23:44:46 +0900

    change heroku.sh

--- heroku.sh
+++ heroku.sh
@@ -1,3 +1,5 @@
-npm run --prefix lol-heimer-frontend build ;\
+cd lol-heimer-frontend ;\
+npm install ;\
+npm run build ;\
 mv ./lol-heimer-frontend/build/index.html ./application/views/home.html ;\
 mv ./lol-heimer-frontend/build/* ./ ;\
