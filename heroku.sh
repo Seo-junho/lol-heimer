aa7056cb88ef7e153908d7ca17b10297e86cd046
zoz0312 <zoz0312@naver.com>
2021-01-08 00:45:36 +0900

    change script

--- heroku.sh
+++ heroku.sh
@@ -1,9 +1,2 @@
-cd lol-heimer-frontend ;\
-echo "start npm install";\
-npm install ;\
-echo "end npm install";\
-echo "================";\
-echo "start build";\
-npm run build ;\
 mv ./lol-heimer-frontend/build/index.html ./application/views/home.html ;\
 mv ./lol-heimer-frontend/build/* ./ ;\
