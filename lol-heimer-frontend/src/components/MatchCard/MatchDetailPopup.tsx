37a50dfc8d73ecacc698ac9bfbe41415e2e95e62
DESKTOP-NUTKK5H\zoz03 <zoz0312@naver.com>
2021-01-13 04:44:39 +0900

    MatchDetail 파일 분할

--- lol-heimer-frontend/src/components/MatchCard/MatchDetailPopup.tsx
+++ lol-heimer-frontend/src/components/MatchCard/MatchDetailPopup.tsx
@@ -1,63 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { API_SEARCH_GET_MATCH_DETAIL } from './../../end-point/index';
-import ItemBox from './ItemBox';
-import MatchDetailUser from './MatchDetailUser';
 import SkeletonMatchDetail from './../../skeleton/SkeletonMatchDetail';
-
-interface IHeaderProps {
-	team: string;
-	className: string;
-	stat: string;
-};
-
-const MatchTeamHeader: React.FC<IHeaderProps> = ({
-	team = 'blue',
-	className = '',
-	stat = '',
-}) => {
-	return (
-		<div className="hidden md:flex justify-start flex-row items-center my-1">
-			<div className="w-60 mr-0.5">
-				<span className={`font-bold ${className}`}>
-					{ stat }
-				</span>&nbsp;({ team === 'blue' ? '블루' : '레드' }팀)
-			</div>
-			<div className="md: w-20 text-center">티어</div>
-			<div className="w-24 text-center">KDA</div>
-			<div className="w-14 text-center">CS</div>
-			<div className="w-28 text-center ml-2.5">피해량</div>
-			<div className="w-48 text-center">아이템</div>
-		</div>
-	);
-};
-
-interface ISectionProps {
-	team: string;
-	data: any[];
-	maxDamage: number;
-};
-
-const MatchTeamSection: React.FC<ISectionProps> = ({
-	team,
-	data,
-	maxDamage,
-}) => {
-
-	const isWin = (stat: string) => (stat === '승리');
-	const bgStyle = team === 'blue' ? 'bg-blue-100 rounded-xl rounded-b-none' : 'bg-red-100';
-
-	return (
-		<div className={`flex justify-center flex-col ${bgStyle} p-2 md:p-10`}>
-			<MatchTeamHeader
-				team={team}
-				className={isWin(data[0].game_stat) ? 'text-blue-600' : 'text-red-600'}
-				stat={data[0].game_stat}
-			/>
-			{ data.map((data: any, index: number) => <MatchDetailUser key={index} user={data} maxDamage={maxDamage} />) }
-		</div>
-	)
-}
+import MatchDetailTeamSection from './MathDetailTeamSection';
 
 interface IProps {
 	gameId: number;
@@ -114,14 +59,14 @@ const MatchDetailPopup: React.FC<IProps> = ({
 				{ isLoading ? <SkeletonMatchDetail /> : (
 					<section className="flex flex-col bg-red-100 rounded-xl">
 						{ blueTeam.length !== 0 && (
-							<MatchTeamSection
+							<MatchDetailTeamSection
 								team={'blue'}
 								data={blueTeam}
 								maxDamage={maxDamage}
 							/>
 						) }
 						{ redTeam.length !== 0 && (
-							<MatchTeamSection
+							<MatchDetailTeamSection
 								team={'red'}
 								data={redTeam}
 								maxDamage={maxDamage}
---
+++ lol-heimer-frontend/src/components/MatchCard/MathDetailTeamSection.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import MatchTeamHeader from './MatchDetailTeamHeader';
+import MatchDetailUser from './MatchDetailUser';
+
+interface ISectionProps {
+	team: string;
+	data: any[];
+	maxDamage: number;
+};
+
+const MatchDetailTeamSection: React.FC<ISectionProps> = ({
+	team,
+	data,
+	maxDamage,
+}) => {
+
+	const isWin = (stat: string) => (stat === '승리');
+	const bgStyle = team === 'blue' ? 'bg-blue-100 rounded-xl rounded-b-none' : 'bg-red-100';
+
+	return (
+		<div className={`flex justify-center flex-col ${bgStyle} p-2 md:p-10`}>
+			<MatchTeamHeader
+				team={team}
+				className={isWin(data[0].game_stat) ? 'text-blue-600' : 'text-red-600'}
+				stat={data[0].game_stat}
+			/>
+			{ data.map((data: any, index: number) => <MatchDetailUser key={index} user={data} maxDamage={maxDamage} />) }
+		</div>
+	)
+}
+
+export default MatchDetailTeamSection;
