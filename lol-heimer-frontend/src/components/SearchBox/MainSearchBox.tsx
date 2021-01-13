4b1aad92e7622662a6756e4e72cbe76e1c056278
DESKTOP-NUTKK5H\zoz03 <zoz0312@naver.com>
2021-01-13 19:24:38 +0900

    Search Box Design fix

--- lol-heimer-frontend/src/components/SearchBox/MainSearchBox.tsx
+++ lol-heimer-frontend/src/components/SearchBox/MainSearchBox.tsx
@@ -35,23 +35,25 @@ const MainSearchBox: React.FC<Props> = (
 
 	return (
 		<article
-			className="w-full px-5 xl:px-0 mt-10 max-w-screen-xl mx-auto"
+			className="w-full px-5 xl:px-0 mt-2 max-w-screen-xl mx-auto"
 		>
 			<form
 				className="flex items-center justify-center h-20"
 				onSubmit={handleSubmit(formSubmit)}
 			>
-				<input
-					ref={register}
-					type="text"
-					name="username"
-					placeholder="소환사 명"
-					className="w-full sm:w-2/5 p-2 focus:outline-none border border-orange-400"
-					required
-				/>
-				<button className="w-20 p-2 text-white border border-orange-400 bg-orange-400 hover:bg-orange-500 transition-colors">
-					검색
-				</button>
+				<div className="w-full sm:w-2/5 flex flex-row shadow-md rounded-sm">
+					<input
+						ref={register}
+						type="text"
+						name="username"
+						placeholder="소환사 명"
+						className="w-full px-3 py-1 text-xl focus:outline-none border border-orange-400 rounded-l-sm"
+						required
+					/>
+					<button className="w-20 p-2 text-white border border-orange-400 bg-orange-400 hover:bg-orange-500 transition-colors rounded-r-sm">
+						검색
+					</button>
+				</div>
 			</form>
 		</article>
 	)
