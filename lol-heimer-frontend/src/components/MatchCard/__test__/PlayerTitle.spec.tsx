6e66242305d6c4260bd74c62ddf1d164802829cc
DESKTOP-NUTKK5H\zoz03 <zoz0312@naver.com>
2021-01-18 00:11:06 +0900

    PlayerTitle Testing Add

--- lol-heimer-frontend/src/components/MatchCard/__test__/PlayerTitle.spec.tsx
+++ lol-heimer-frontend/src/components/MatchCard/__test__/PlayerTitle.spec.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import PlayerTitle from '../PlayerTitle';
-import { render, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('<PlayerTitle />', () => {
-	it.todo('PlayerTitle Render Done')
+	it('PlayerTitle Render Done', () => {
+		const { getByText } = render(
+			<PlayerTitle
+				championId={1000}
+				summonerName={'챔편이름'}
+			/>
+		);
+		getByText(/1000/gi);
+		getByText(/챔편이름/gi);
+	})
 });
