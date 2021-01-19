2117e8b5305914e25627b919ee1b20463dc2d845
jun_ho98 <jun_ho98@naver.com>
2021-01-19 21:04:33 +0900

    mysql test

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -3,13 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Test extends CI_Controller {
 	public function index(){
-		$mc = new Memcached();
-		$mc->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
-		$mc->addServers(array_map(function($server) { return explode(':', $server, 2); }, explode(',', MEMCACHEDCLOUD_SERVERS)));
-		$mc->setSaslAuthData(MEMCACHEDCLOUD_USERNAME,MEMCACHEDCLOUD_PASSWORD);
-
-		$mc->set('welcome_msg', 'Hello from Redis!');
-		$result = $mc->get('welcome_msg');
-		var_dump($result);
+		$db = $this->load->database();
+		$a = $db->query("SELECT * FROM env_config ");
+		var_dump($a);
 	}
 }
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -2,7 +2,7 @@
 class MY_Controller extends CI_Controller {
 	function __construct() {
 		parent::__construct();
-		//$db = $this->load->database('heimer', TRUE);
+		//$db = $this->load->database();
 		//$a = $db->query("SELECT * FROM env_config ");
 		//var_dump($a);
 	}
