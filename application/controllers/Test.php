14bdad16ea15c1c8b59ad4fceea8f652efd87476
jun_ho98 <jun_ho98@naver.com>
2021-01-19 21:36:25 +0900

    Update Test.php

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Test extends CI_Controller {
 	public function index(){
-		$this->db = $this->load->database();
+		$this->load->database();
 		$a = $this->db->query("SELECT * FROM env_config ");
 		var_dump($a);
 	}
