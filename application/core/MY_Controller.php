249e66e6d6765171931682c62b0809d40e51dc4d
jun_ho98 <jun_ho98@naver.com>
2021-01-19 20:42:49 +0900

    mysql test

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class MY_Controller extends CI_Controller {
+	function __construct() {
+		parent::__construct();
+		$db = $this->load->database('heimer', TRUE);
+		$a = $db->query("SELECT * FROM env_config ");
+		var_dump($a);
+	}
+}
